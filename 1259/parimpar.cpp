295e187ad16102f4149126460ad83f0214d2af42
guilhermeggloor <guilhermegloor8@gmail.com>
2025-03-26 10:43:35 -0400

    feat: exercise 1259 resolved

--- 1259/parimpar.cpp
+++ 1259/parimpar.cpp
@@ -1,10 +1,43 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
+
+using namespace std;
 
 // Entrada: unica linha de entrada com inteiro N (1 < N <= 10⁵)
 
 int main()
 {
+    int N;
+
+    cin >> N;
+
+    vector<int> pares, impares;
+
+    for (int i = 0; i < N; i++)
+    {
+        int numero;
+        cin >> numero;
+
+        if (numero % 2 == 0)
+        {
+            pares.push_back(numero);
+        }
+        else
+        {
+            impares.push_back(numero);
+        }
+    }
+
+    sort(pares.begin(), pares.end());
+
+    sort(impares.rbegin(), impares.rend());
+
+    for (int num : pares)
+        cout << num << endl;
+
+    for (int num : impares)
+        cout << num << endl;
 
     return 0;
 }
